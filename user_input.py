8e36618775d470986cf8d0da0c1270f4a67bcd30
rjjunior <168284317+rjjohny@users.noreply.github.com>
2024-08-18 21:33:40 +0300

    Add user_input.py file

---
+++ user_input.py
@@ -0,0 +1,8 @@
+# ask the user for thier name 
+name = input("what is your name ")
+# ask the user for thier age 
+age = input("what is your age ")
+#ask the user for thier location 
+location = input("what is your location ")
+#print a personalized message 
+print =(f"Hello{name},you are{age}years ols and you live in {location}")
